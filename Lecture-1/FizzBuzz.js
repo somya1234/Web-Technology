5bfbc508df52a6ddb2bd2cbf629c278a70fe0e01
Somya <somya1234@gmail.com>
2020-01-17 09:59:40 +0530

    solved decimalToBinary problem

---
+++ Lecture-1/FizzBuzz.js
@@ -0,0 +1,16 @@
+for(var i=1; i<=20; i++){
+    if(i%3 == 0 && i%5 == 0){
+        console.log('FizzBuzz');
+    } else if (i%3==0){
+        console.log('Fizz');
+    }
+    else if(i%5 == 0){
+        console.log('Buzz');
+    }
+    else if(i%3 == 0 && i%5 == 0){
+        console.log('FizzBuzz');
+    }
+    else{
+        console.log(i);
+    }
+}
